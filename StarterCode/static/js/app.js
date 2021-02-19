c44bd6a9009f1d594c57f6de8e318069bd7e8b86
Ran Wei <harryweebb@gmail.com>
2021-02-19 16:24:35 -0700

    Finished bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,12 +6,35 @@ d3.json("samples.json").then(function(data) {
         y: sample.otu_ids.slice(0,10).map(i => 'OTU ' + i),
         orientation: 'h'
     }];
-    var layout = {
+    var hbarLayout = {
         yaxis: {
             autorange: 'reversed'
         }
     }
-    console.log(sample.otu_ids.slice(0,10).map(i => 'OTU ' + i))
-    Plotly.newPlot('bar', hbar, layout);
+    Plotly.newPlot('bar', hbar, hbarLayout);
+
+    console.log(sample);
+    var bubble = [{
+        x: sample.otu_ids,
+        y: sample.sample_values,
+        text: sample.otu_labels,
+        mode: 'markers',
+        marker: {
+            size: sample.sample_values,
+            sizeref: 0.1,
+            sizemode: 'area',
+            color: sample.otu_ids,
+            colorscale: [[0, '#1f77b4'],[0.5, '#2ca02c'],[1, '#8c564b']],
+            opacity: 0.7
+        }
+    }];
+    var bbLayout = {
+        xaxis: {
+            title:{
+                text: 'OTU ID'
+            }
+        }
+      };
+      Plotly.newPlot('bubble', bubble, bbLayout);
 })
 
